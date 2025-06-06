eee56e92cdbce63190e84c9886b6b4004e318f45
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2025-06-06 18:11:54 -0500

    commit first part of rewrite

--- src/main.zig
+++ src/main.zig
@@ -1,237 +1,224 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    const debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 
-    var code_file: ?std.fs.File = null;
-    defer if (code_file) |file|
-        file.close();
+    const allocator, const is_debug = gpa: {
+        if (builtin.os.tag == .wasi) break :gpa .{ std.heap.wasm_allocator, false };
+        break :gpa switch (builtin.mode) {
+            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
+        };
+    };
+    defer if (is_debug) {
+        _ = debug_allocator.deinit();
+    };
 
-    var byte_output = false;
-
-    var args = try std.process.argsWithAllocator(allocator);
+    const args: Args = try .parse(allocator);
     defer args.deinit();
 
-    const arg0 = args.next() orelse
-        return error.NoArgsGiven;
-
-    while (args.next()) |arg| {
-        if (std.mem.startsWith(u8, arg, "-")) {
-            for (arg[1..]) |byte| {
-                switch (byte) {
-                    'b' => byte_output = true,
-                    else => {
-                        try printUsage(arg0);
-                        return error.BadFlags;
-                    },
-                }
-            }
-        } else if (code_file == null) {
-            code_file = try std.fs.cwd().openFile(arg, .{});
-        } else {
-            try printUsage(arg0);
-            return error.TooManyCodeFiles;
-        }
-    }
-
-    if (code_file) |code| {
-        try interpret(code, byte_output, allocator);
-    } else {
-        try printUsage(arg0);
-        return error.NoCodeFileGiven;
+    if (args.options.help) {
+        try printHelp(args.arg0, std.io.getStdOut());
     }
 }
 
-fn printUsage(arg0: []const u8) !void {
-    const stderr = std.io.getStdErr();
-
-    try stderr.writer().print(
-        \\usage: {s} <code file> [-b]
-        \\
-        \\flags:
-        \\  -b        print output as bytes
-        \\
-    , .{arg0});
-    return error.BadArgCount;
-}
-
-const ActiveCommands = struct {
+const Instruction = packed struct(u8) {
     increment_id: bool,
     decrement_id: bool,
     increment_mp: bool,
     decrement_mp: bool,
 
-    increment_offset: bool = false,
-    decrement_offset: bool = false,
-
-    swap_mode: bool = false,
-
     flip_bit: bool,
     output_bit: bool,
+
     set_checkpoint: bool,
-    get_checkpoint: bool,
+    load_checkpoint: bool,
 
-    fn init(byte: u8) ActiveCommands {
-        var result = ActiveCommands{
-            .increment_id = byte & 0b1000_0000 != 0,
-            .decrement_id = byte & 0b0100_0000 != 0,
-            .increment_mp = byte & 0b0010_0000 != 0,
-            .decrement_mp = byte & 0b0001_0000 != 0,
+    fn increment_offset(instruction: Instruction) bool {
+        return instruction.increment_id and instruction.decrement_id;
+    }
 
-            .flip_bit = byte & 0b0000_1000 != 0,
-            .output_bit = byte & 0b0000_0100 != 0,
-            .set_checkpoint = byte & 0b0000_0010 != 0,
-            .get_checkpoint = byte & 0b0000_0001 != 0,
+    fn decrement_offset(instruction: Instruction) bool {
+        return instruction.increment_mp and instruction.decrement_mp;
+    }
+
+    fn swap_mode(instruction: Instruction) bool {
+        return instruction.increment_offset() and instruction.decrement_offset();
+    }
+
+    /// returns null on Eof
+    fn read(reader: anytype) !?Instruction {
+        const byte = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => return null,
+            else => return err,
         };
 
-        result.parseOpposites();
-
-        return result;
-    }
-
-    fn parseOpposites(self: *ActiveCommands) void {
-        self.swap_mode = self.increment_id and self.decrement_id and
-            self.increment_mp and self.decrement_mp;
-
-        if (!self.swap_mode) {
-            self.increment_offset = self.increment_id and self.decrement_id;
-            self.decrement_offset = self.increment_mp and self.decrement_mp;
-        }
+        return @bitCast(byte);
     }
 };
 
-const Mode = enum(u1) {
-    pc,
-    mp,
+const Register = struct { // could use a better name
+    ptr: usize,
+    id: isize,
+    offset: isize,
+    checkpoints: std.AutoHashMap(isize, isize),
+
+    fn init(allocator: std.mem.Allocator) Register {
+        return .{
+            .ptr = 0,
+            .id = 0,
+            .offset = 0,
+            .checkpoints = .init(allocator),
+        };
+    }
+
+    fn deinit(counter: Register) void {
+        counter.checkpoints.deinit();
+    }
 };
 
-fn interpret(code_file: std.fs.File, byte_output: bool, allocator: std.mem.Allocator) !void {
-    const code_reader = code_file.reader();
-    const stdout = std.io.getStdOut().writer();
+fn interpret(
+    code_file: std.fs.File,
+    allocator: std.mem.Allocator,
+) !void {
+    var pc: Register = .init(allocator);
+    defer pc.deinit();
 
-    // only used when byte_output is true,
-    var output_byte: u8 = 0;
+    var mp: Register = .init(allocator);
+    defer mp.deinit();
 
-    var output_bits_sent: u3 = 0;
-    var did_output: bool = false;
+    var buffered_code = std.io.bufferedReader(code_file.reader());
+    const code = buffered_code.reader();
 
-    defer if (output_bits_sent > 0)
-        stdout.writeByte(output_byte) catch {}; // can't return error in defer
-
-    defer if (!byte_output and did_output)
-        stdout.writeByte('\n') catch {};
-
-    var pc_checkpoints = std.AutoArrayHashMap(isize, isize).init(allocator);
-    defer pc_checkpoints.deinit();
-
-    var mp_checkpoints = std.AutoArrayHashMap(isize, isize).init(allocator);
-    defer mp_checkpoints.deinit();
-
-    var memory = std.AutoArrayHashMap(usize, u1).init(allocator);
-    defer memory.deinit();
-
-    var memory_pointer: usize = 0;
-
-    var checkpoint_id: isize = 0;
-    var checkpoint_offset: isize = 0;
-    var checkpoint_mode: Mode = .pc;
-
-    var active_commands: ActiveCommands = undefined;
-
-    while (code_reader.readByte() catch null) |byte| {
-        active_commands = ActiveCommands.init(byte);
-
-        if (active_commands.swap_mode) {
-            checkpoint_mode = @enumFromInt(@intFromEnum(checkpoint_mode) -% 1);
-        } else if (active_commands.increment_offset) {
-            checkpoint_offset += 1;
-        } else if (active_commands.decrement_offset) {
-            checkpoint_offset -= 1;
+    while (try Instruction.read(code)) |instruction| {
+        if (instruction.swap_mode()) {
+            //
+        } else if (instruction.increment_offset()) {
+            //
+        } else if (instruction.decrement_offset()) {
+            //
         } else {
-            if (active_commands.increment_id) {
-                checkpoint_id += 1;
-            } else if (active_commands.decrement_id) {
-                checkpoint_id -= 1;
+            if (instruction.increment_id) {
+                //
+            } else if (instruction.decrement_id) {
+                //
             }
 
-            if (active_commands.increment_mp) {
-                memory_pointer += 1;
-            } else if (active_commands.decrement_mp) {
-                if (memory_pointer == 0) return;
-
-                memory_pointer -= 1;
+            if (instruction.increment_mp) {
+                //
+            } else if (instruction.decrement_mp) {
+                //
             }
         }
 
-        if (active_commands.flip_bit) {
-            const entry = try memory.getOrPut(memory_pointer);
-
-            if (entry.found_existing) {
-                entry.value_ptr.* = ~entry.value_ptr.*;
-            } else {
-                entry.value_ptr.* = 1;
-            }
+        if (instruction.flip_bit) {
+            //
         }
 
-        if (active_commands.output_bit) {
-            if (byte_output) {
-                output_byte <<= 1;
-                output_byte |= memory.get(memory_pointer) orelse 0;
-
-                if (output_bits_sent == 7) {
-                    try stdout.writeByte(output_byte);
-                }
-
-                output_bits_sent +%= 1;
-            } else {
-                try stdout.print("{d}", .{
-                    memory.get(memory_pointer) orelse 0,
-                });
-
-                if (output_bits_sent == 7)
-                    try stdout.writeByte(' ');
-
-                output_bits_sent +%= 1;
-            }
-            did_output = true;
+        if (instruction.output_bit) {
+            //
         }
-        if (active_commands.set_checkpoint) {
-            if (checkpoint_mode == .pc) {
-                try pc_checkpoints.put(
-                    checkpoint_id,
-                    // position is after the currently executing commands
-                    @as(i64, @intCast(try code_file.getPos())) + checkpoint_offset,
-                );
-            } else {
-                try mp_checkpoints.put(
-                    checkpoint_id,
-                    @as(i64, @intCast(memory_pointer)) + checkpoint_offset,
-                );
-            }
+
+        if (instruction.set_checkpoint) {
+            //
         }
-        if (active_commands.get_checkpoint) {
-            if ((memory.get(memory_pointer) orelse 0) == 1) {
-                if (checkpoint_mode == .pc) {
-                    const checkpoint = pc_checkpoints.get(checkpoint_id) orelse
-                        return error.ReadFromUndefinedCheckpoint;
 
-                    if (checkpoint < 0)
-                        return;
-
-                    // no need to add 1 since that was done when setting
-                    try code_file.seekTo(@intCast(checkpoint));
-                } else {
-                    const checkpoint = mp_checkpoints.get(checkpoint_id) orelse
-                        return error.ReadFromUndefinedCheckpoint;
-
-                    if (checkpoint < 0)
-                        return;
-
-                    memory_pointer = @intCast(checkpoint);
-                }
-            }
+        if (instruction.load_checkpoint) {
+            //
         }
     }
+}
+
+const Args = struct {
+    arg0: []const u8,
+    optional_file: ?std.fs.File,
+    options: Options,
+
+    const Options = struct {
+        /// if true, bits are printed in groups of 8 as characters.
+        /// if false, bits are printed one at a time as either a '1' or a '0'
+        bytes: bool,
+        /// if true, each command executed is logged in textual form
+        log: bool,
+        /// print help and exit
+        help: bool,
+
+        const none: Options = .{
+            .bytes = false,
+            .log = false,
+            .help = false,
+        };
+    };
+
+    /// result is owned by the caller and must be deinitialized after use
+    fn parse(allocator: std.mem.Allocator) !Args {
+        var args = try std.process.argsWithAllocator(allocator);
+        defer args.deinit();
+
+        const arg0 = args.next() orelse
+            return error.NoArg0;
+
+        var optional_filename: ?[]const u8 = null;
+        var options: Args.Options = .none;
+
+        while (args.next()) |arg| {
+            if (std.mem.startsWith(u8, arg, "--")) {
+                if (std.mem.eql(u8, arg[2..], "bytes"))
+                    options.bytes = true
+                else if (std.mem.eql(u8, arg[2..], "log"))
+                    options.log = true
+                else if (std.mem.eql(u8, arg[2..], "help"))
+                    options.help = true
+                else
+                    return error.UnknownOption;
+            } else if (arg.len >= 2 and arg[0] == '-') {
+                for (arg[1..]) |flag| {
+                    switch (flag) {
+                        'b' => options.bytes = true,
+                        'l' => options.log = true,
+                        'h' => options.help = true,
+                        else => return error.UnknownFlag,
+                    }
+                }
+            } else if (optional_filename == null) {
+                optional_filename = arg;
+            } else return error.TooManyFiles;
+        }
+
+        if (optional_filename == null and !options.help) {
+            try printHelp(arg0, std.io.getStdErr());
+            return error.NoFileSpecified;
+        }
+        const optional_file = if (optional_filename) |filename|
+            try std.fs.cwd().openFile(filename, .{})
+        else
+            null;
+
+        return .{
+            .arg0 = try allocator.dupe(u8, arg0),
+            .optional_file = optional_file,
+            .options = options,
+        };
+    }
+
+    fn deinit(args: Args, allocator: std.mem.Allocator) void {
+        allocator.free(args);
+
+        if (args.optional_file) |file| {
+            file.close();
+        }
+    }
+};
+
+fn printHelp(arg0: []const u8, output: std.fs.File) !void {
+    try output.writer().print(
+        \\usage: {s} <file> [options]
+        \\
+        \\options:
+        \\  -b  --bytes     group output into bytes and print as characters
+        \\  -l  --log       print each instruction as it is run
+        \\  -h  --help      print help and exit
+        \\
+    , .{arg0});
 }
